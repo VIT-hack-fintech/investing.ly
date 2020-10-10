122392e4e5c81f6432624c0b58891f5ed07a8e92
preet <preet.patel2806@gmail.com>
2020-10-10 16:37:54 +0530

    me

--- home.js
+++ home.js
@@ -1,27 +1,37 @@
 const playStocks = () =>{
-    var x = document.getElementById("svgStocks").children;
+    var x = document.getElementById("svgStocks").getElementsByClassName("grow");
     var i;
     for (i = 0; i < x.length; i++) {
-        x[i].style.animationPlayState="running";
+        x[i].style.animation="bargrow 0.7s ease-out 2 alternate backwards";
+    }
+    var x = document.getElementById("svgStocks").getElementsByClassName("shrink");
+    var i;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.animation="barshrink 0.7s ease-out 2 alternate backwards";
+    }
+    var x = document.getElementById("svgStocks").getElementsByClassName("grow2");
+    var i;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.animation="bargrow2 0.7s ease-out 2 alternate backwards";
     }
 }
 const pauseStocks = () =>{
     var x = document.getElementById("svgStocks").children;
     var i;
     for (i = 0; i < x.length; i++) {
-        x[i].style.animationPlayState="paused";
+        x[i].style.animation="none";
     }
-
 }
 const playFunds = () =>{
-    document.getElementById("svgFunds").style.animationPlayState="running";
+    document.getElementById("svgFunds").style.animation="wiggle 1s linear 1 normal backwards";
+    
 }
 const pauseFunds = () =>{
-    document.getElementById("svgFunds").style.animationPlayState="paused";
+    document.getElementById("svgFunds").style.animation="none";
 }
 const playCrypto = () =>{
-    document.getElementById("svgCrypto").style.animationPlayState="running";
+    document.getElementById("svgCrypto").style.animation="bounce 1s linear 1 normal backwards";
 }
 const pauseCrypto = () =>{
-    document.getElementById("svgCrypto").style.animationPlayState="paused";
+    document.getElementById("svgCrypto").style.animation="none";
 }
