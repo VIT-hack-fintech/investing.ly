ffedc7f706668a8421f70836ed79622112af9b5b
chirayurathi <chirayurathi4@gmail.com>
2020-10-10 18:22:06 +0530

    home page complete

--- script.js
+++ script.js
@@ -13,3 +13,21 @@ window.addEventListener("scroll",()=>{
         document.querySelector("nav").style.height = "15vh";
     }
 })
+document.querySelector('.hamburger').onclick = ()=>{
+    document.querySelector('nav').classList.toggle('nav-active');
+    document.querySelector('.modal').classList.toggle('mod-active');
+}
+document.querySelector('.modal').onclick = ()=>{
+    document.querySelector('nav').classList.toggle('nav-active');
+    document.querySelector('.modal').classList.toggle('mod-active');
+}
+function parallax() {
+    var s = document.querySelector(".landing");
+  var yPos = 0 - window.pageYOffset/4;	
+  s.style.top = 0 + yPos + "%";
+  //alert(yPos);
+}
+
+window.addEventListener("scroll", function(){
+	parallax();	
+});
